cecc59c05897a5d881515ddc4e229c6f3f6925fe
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2025-02-18 11:19:28 +0000

    Begin implementation of test_model

--- test.py
+++ test.py
@@ -1,8 +1,24 @@
 
-
 from pandas import DataFrame
 
+from util import *
 
-def test_model(model: DataFrame, data: DataFrame) -> str:
+
+def calculate_chance() -> DataFrame:
+    raise NotImplementedError()
+
+
+def test_model(
+    model: DataFrame,
+    data: DataFrame,
+    stop_words: list[str]
+) -> DataFrame:
+
     # TODO: Implement
-    return ""
+    # Get overall classification chance
+    classification_chance = model[[CLASSIFICATION_COL_FMT], [CHANCE_COL_FMT]]
+
+    print(classification_chance)
+
+    raise NotImplementedError()
+    # Use overall word classifications
